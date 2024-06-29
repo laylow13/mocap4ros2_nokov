46c367a9b82ae9217167d1cba1dd4edb82841855
laylow13 <13148275260@163.com>
2024-06-29 23:15:16 +0800

    basic implementation

--- mocap4r2_nokov_driver/include/mocap4r2_nokov_driver/mocap4r2_nokov_driver.hpp
+++ mocap4r2_nokov_driver/include/mocap4r2_nokov_driver/mocap4r2_nokov_driver.hpp
@@ -14,6 +14,8 @@
 #include <chrono>
 #include <vector>
 
+#include "NokovSDKCAPI.h"
+#include "NokovSDKClient.h"
 
 #include "rclcpp/rclcpp.hpp"
 #include "mocap4r2_control/ControlledLifecycleNode.hpp"
@@ -22,28 +24,36 @@
 #include "mocap4r2_msgs/msg/markers.hpp"
 #include "mocap4r2_msgs/msg/rigid_bodies.hpp"
 
-namespace mocap4r2_nokov_driver
-{
+namespace mocap4r2_nokov_driver {
 
-    class NokovDriverNode : public mocap4r2_control::ControlledLifecycleNode
-    {
+    class NokovDriverNode : public mocap4r2_control::ControlledLifecycleNode {
     public:
         NokovDriverNode();
 
         using CallbackReturnT =
                 rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn;
 
-        CallbackReturnT on_configure(const rclcpp_lifecycle::State & state);
-        CallbackReturnT on_activate(const rclcpp_lifecycle::State & state);
-        CallbackReturnT on_deactivate(const rclcpp_lifecycle::State & state);
-        CallbackReturnT on_cleanup(const rclcpp_lifecycle::State & state);
-        CallbackReturnT on_shutdown(const rclcpp_lifecycle::State & state);
-        CallbackReturnT on_error(const rclcpp_lifecycle::State & state);
+        CallbackReturnT on_configure(const rclcpp_lifecycle::State &state);
+
+        CallbackReturnT on_activate(const rclcpp_lifecycle::State &state);
+
+        CallbackReturnT on_deactivate(const rclcpp_lifecycle::State &state);
+
+        CallbackReturnT on_cleanup(const rclcpp_lifecycle::State &state);
+
+        CallbackReturnT on_shutdown(const rclcpp_lifecycle::State &state);
+
+        CallbackReturnT on_error(const rclcpp_lifecycle::State &state);
+
         bool connect_nokov();
+
         void set_settings_nokov();
+
         void initParameters();
 
     protected:
+        std::unique_ptr<NokovSDKClient> nokovClient;
+
         rclcpp_lifecycle::LifecyclePublisher<mocap4r2_msgs::msg::Markers>::SharedPtr markers_pub_;
         rclcpp_lifecycle::LifecyclePublisher<mocap4r2_msgs::msg::RigidBodies>::SharedPtr
                 rigid_bodies_pub_;
@@ -52,11 +62,12 @@ namespace mocap4r2_nokov_driver
         std::string stream_mode_;
         std::string host_name_;
         std::string frame_id_;
-        int frameCount_ {0};
+        int frameCount_{0};
 
         void process_frame();
 
         void control_start(const mocap4r2_control_msgs::msg::Control::SharedPtr msg) override;
+
         void control_stop(const mocap4r2_control_msgs::msg::Control::SharedPtr msg) override;
     };
 
--- mocap4r2_nokov_driver/src/mocap4r2_nokov_driver/mocap4r2_nokov_driver.cpp
+++ mocap4r2_nokov_driver/src/mocap4r2_nokov_driver/mocap4r2_nokov_driver.cpp
@@ -7,17 +7,74 @@
 using namespace std::chrono_literals;
 
 namespace mocap4r2_nokov_driver {
+    struct sFrameData {
+        mocap4r2_msgs::msg::Markers markers;
+        mocap4r2_msgs::msg::RigidBodies rigidBodies;
+    } frameData;
+    std::mutex mtx;
 
-// The vicon driver node has differents parameters
-// to initialized with the mocap4r2_vicon_driver_params.yaml
-    NokovDriverNode::NokovDriverNode()
-            : ControlledLifecycleNode("mocap4r2_vicon_driver_node") {
-        declare_parameter<std::string>("stream_mode", "ClientPull");
-        declare_parameter<std::string>("host_name", "192.168.10.1:801");
-        declare_parameter<std::string>("frame_id", "vicon_world");
+    void DataHandler(sFrameOfMocapData *pFrameOfData, void *pUserData) {
+        if (nullptr == pFrameOfData)
+            return;
+        // Store the frame
+        std::lock_guard<std::mutex> lck(mtx);
+
+        int nmarker = (pFrameOfData->nOtherMarkers < MAX_MARKERS) ? pFrameOfData->nOtherMarkers : MAX_MARKERS;
+
+        frameData.markers.frame_number = pFrameOfData->iFrame;
+        for (int i = 0; i < nmarker; ++i) {
+            mocap4r2_msgs::msg::Marker marker;
+            marker.id_type = mocap4r2_msgs::msg::Marker::USE_INDEX;
+//            marker.marker_index =; //TODO:
+            marker.translation.x = pFrameOfData->OtherMarkers[i][0];
+            marker.translation.y = pFrameOfData->OtherMarkers[i][1];
+            marker.translation.z = pFrameOfData->OtherMarkers[i][2];
+            frameData.markers.markers.push_back(marker);
+        }
+
+
+        frameData.rigidBodies.frame_number = pFrameOfData->iFrame;
+        for (int i = 0; i < pFrameOfData->nRigidBodies; ++i) {
+            mocap4r2_msgs::msg::RigidBody rigidBody;
+            auto &rigidBodyData = pFrameOfData->RigidBodies[i];
+            rigidBody.rigid_body_name = rigidBodyData.ID;
+            rigidBody.pose.position.x = rigidBodyData.x;
+            rigidBody.pose.position.y = rigidBodyData.y;
+            rigidBody.pose.position.z = rigidBodyData.z;
+            rigidBody.pose.orientation.w = rigidBodyData.qx;
+            rigidBody.pose.orientation.x = rigidBodyData.qy;
+            rigidBody.pose.orientation.y = rigidBodyData.qz;
+            rigidBody.pose.orientation.z = rigidBodyData.qw;
+            auto nMarkerofBody = rigidBodyData.nMarkers;
+            for (size_t j = 0; j < nMarkerofBody; j++) {
+                mocap4r2_msgs::msg::Marker marker;
+                marker.id_type = mocap4r2_msgs::msg::Marker::USE_INDEX;
+                marker.marker_index = rigidBodyData.MarkerIDs[j];
+                marker.translation.x = rigidBodyData.Markers[j][0];
+                marker.translation.y = rigidBodyData.Markers[j][1];
+                marker.translation.z = rigidBodyData.Markers[j][2];
+                rigidBody.markers.push_back(marker);
+            }
+            frameData.rigidBodies.rigidbodies.push_back(rigidBody);
+        }
     }
 
-// In charge of choose the different driver options related and provided by the Vicon SDK
+    const sFrameData &GetCurrentFrame() {
+        static sFrameData frame;
+        {
+            std::lock_guard<std::mutex> lck(mtx);
+            frame = frameData;
+        }
+        return frame;
+    }
+
+    NokovDriverNode::NokovDriverNode()
+            : ControlledLifecycleNode("mocap4r2_nokov_driver_node") {
+        declare_parameter<std::string>("host_name", "192.168.10.1:801");
+        declare_parameter<std::string>("frame_id", "nokov_world");
+    }
+
+// In charge of choose the different driver options related and provided by the Nokov SDK
     void NokovDriverNode::set_settings_nokov() {
     }
 
@@ -31,9 +88,22 @@ namespace mocap4r2_nokov_driver {
         (void) msg;
     }
 
-// In charge of get the Vicon information and convert it to vicon_msgs
     void NokovDriverNode::process_frame() {
-
+        if (markers_pub_->get_subscription_count() == 0 &&
+            rigid_bodies_pub_->get_subscription_count() == 0) {
+            return;
+        }
+        auto frame = GetCurrentFrame();
+        frame.markers.header.stamp = now();
+        frame.markers.header.frame_id = frame_id_;
+        frame.rigidBodies.header.stamp = now();
+        frame.rigidBodies.header.frame_id = frame_id_;
+        if (!frame.markers.markers.empty()) {
+            markers_pub_->publish(frame.markers);
+        }
+        if (!frame.rigidBodies.rigidbodies.empty()) {
+            rigid_bodies_pub_->publish(frame.rigidBodies);
+        }
     }
 
     using CallbackReturnT =
@@ -44,34 +114,73 @@ namespace mocap4r2_nokov_driver {
 
     CallbackReturnT
     NokovDriverNode::on_configure(const rclcpp_lifecycle::State &) {
+        initParameters();
 
+        markers_pub_ = create_publisher<mocap4r2_msgs::msg::Markers>("/markers", rclcpp::QoS(1000));
+        rigid_bodies_pub_ = create_publisher<mocap4r2_msgs::msg::RigidBodies>(
+                "/rigid_bodies", rclcpp::QoS(1000));
+
+        nokovClient = std::make_unique<NokovSDKClient>();
+        nokovClient->SetDataCallback(DataHandler);
+        unsigned char sdkVersion[4] = {0};
+        nokovClient->NokovSDKVersion(sdkVersion);
+        RCLCPP_INFO(get_logger(), "SDK Ver:%c.%c.%c.%c", sdkVersion[0], sdkVersion[1], sdkVersion[2], sdkVersion[3]);
+        // Check the ret value
+        int retValue = nokovClient->Initialize((char *) host_name_.c_str());
+        if (ErrorCode_OK != retValue) {
+            RCLCPP_ERROR(get_logger(), "Error connecting XINGYING on address: %s,Code: %d", host_name_.c_str(),
+                         retValue);
+            RCLCPP_ERROR(get_logger(), "... not connected");
+            return CallbackReturnT::FAILURE;
+        } else {
+            RCLCPP_INFO(get_logger(), "... connected!");
+            return CallbackReturnT::SUCCESS;
+        }
     }
 
     CallbackReturnT
     NokovDriverNode::on_activate(const rclcpp_lifecycle::State &) {
+        markers_pub_->on_activate();
+        rigid_bodies_pub_->on_activate();
 
+        timer_ = create_wall_timer(10ms, std::bind(&NokovDriverNode::process_frame, this));
+        return CallbackReturnT::SUCCESS;
     }
 
     CallbackReturnT
     NokovDriverNode::on_deactivate(const rclcpp_lifecycle::State &) {
+        markers_pub_->on_deactivate();
+        rigid_bodies_pub_->on_deactivate();
 
+        //client disable data
+
+        timer_ = nullptr;
+        return CallbackReturnT::SUCCESS;
     }
 
     CallbackReturnT
     NokovDriverNode::on_cleanup(const rclcpp_lifecycle::State &) {
+//        Disconnect
+        return CallbackReturnT::SUCCESS;
     }
 
     CallbackReturnT
     NokovDriverNode::on_shutdown(const rclcpp_lifecycle::State &) {
+        //        Disconnect
+        return CallbackReturnT::SUCCESS;
     }
 
     CallbackReturnT
     NokovDriverNode::on_error(const rclcpp_lifecycle::State &) {
+        //        Disconnect
+        return CallbackReturnT::SUCCESS;
     }
 
 
 // Init the necessary parameters
     void NokovDriverNode::initParameters() {
+        get_parameter<std::string>("host_name", host_name_);
+        get_parameter<std::string>("frame_id", frame_id_);
     }
 
 }  // namespace mocap4r2_nokov_driver
